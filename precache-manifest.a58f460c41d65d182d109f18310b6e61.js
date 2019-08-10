0752066ec8ab029402b6cd4cf0cc49726f2b13aa
Emmanuel <emmanuel@climbsmedia.com>
2019-08-10 23:35:01 +0200

    Updates

---
+++ precache-manifest.a58f460c41d65d182d109f18310b6e61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf2212a3ad66ea774d07a5c1c42cdd",
+    "url": "/monsterDex/index.html"
+  },
+  {
+    "revision": "cb6d9521de6fadadf779",
+    "url": "/monsterDex/static/css/main.fd0a28dd.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsterDex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "cb6d9521de6fadadf779",
+    "url": "/monsterDex/static/js/main.8b7270c0.chunk.js"
+  },
+  {
+    "revision": "a99b5b276d971056eb54",
+    "url": "/monsterDex/static/js/runtime~main.08cd8a2e.js"
+  }
+]);
